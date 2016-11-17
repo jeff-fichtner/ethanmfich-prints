2be5cbce55b10a1a04af4597c0ba5f35393a872f
Jeff Fichtner <jff2501@gmail.com>
2016-11-17 10:57:10 -0600

    Customize migrations: add name

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20161114162708) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
