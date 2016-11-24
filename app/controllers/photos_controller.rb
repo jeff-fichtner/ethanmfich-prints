aa28898a97e07eefe7f950976146ceed2561ec36
Jeff Fichtner <jff2501@gmail.com>
2016-11-24 12:07:07 -0600

    Jquery plugin, admin routes

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,26 @@
 class PhotosController < ApplicationController
+	before_action :set_s3_direct_post, only: [:new, :edit, :create, :update]
+
 	def index
 	end
 
 	def show
 	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+	def set_s3_direct_post
+    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
   resources :photos, only: [:index, :show]
+  resources :admins do
+		resources :photos, except: [:show]
+  end
   resources :users do
   	resources :orders, except: :destroy
   end
