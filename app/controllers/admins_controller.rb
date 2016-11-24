2e03125cfb81550e447efdde2e2b7368c24de297
Jeff Fichtner <jff2501@gmail.com>
2016-11-23 20:27:34 -0600

    Adding route methods

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,2 +1,24 @@
 class AdminsController < ApplicationController
+	before_action :authenticate_admin!
+
+	def index
+	end
+
+	def create
+	end
+
+	def new
+	end
+
+	def edit
+	end
+
+	def show
+	end
+
+	def update
+	end
+
+	def destroy
+	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,21 @@
 class UsersController < ApplicationController
-	def index
-	end
+	before_action :authenticate_user!
+
 	def create
 	end
+
 	def new
 	end
+
 	def edit
 	end
+
 	def show
 	end
+
 	def update
 	end
+
 	def destroy
 	end
 end
